2709e428c36c003c78b9cc8decbd31c7d19856b2
David Bayendor <dbayendor@gmail.com>
2014-09-30 10:29:43 -0600

    Add zip code and name lookup

--- lib/eventreporter.rb
+++ lib/eventreporter.rb
@@ -10,4 +10,19 @@ class EventReporter
   def initialize(repository = Repository.load_entries('./data'))
     @repository = repository
   end
+
+
+  def lookup(name)
+    lastname, firstname = name.split(', ')
+    if firstname
+      repository.find_by_first_and_last_name(firstname, lastname)
+    else
+      repository.find_by_last_name(name)
+    end
+  end
+
+  def zip_code_lookup(zipcode)
+    repository.find_by_zipcode(zipcode)
+  end
+
 end

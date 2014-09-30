2fc683e6a123c132cdfefef9092fd1a2086a9c7a
David Bayendor <dbayendor@gmail.com>
2014-09-30 08:11:47 -0600

    Update symbols returned from header row of csv

--- lib/entry.rb
+++ lib/entry.rb
@@ -10,15 +10,15 @@ class Entry
               :zipcode
 
   def initialize(data)
-    @regdate = data[:RegDate]
-    @first_name = data[:first_Name]
-    @last_name = data[:last_Name]
-    @email_address = data[:Email_Address]
-    @phone_number = data[:HomePhone]
-    @street = data[:Street]
-    @city = data[:City]
-    @state = data[:State]
-    @zipcode = data[:Zipcode]
+    @regdate = data[:regdate]
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @email_address = data[:email_address]
+    @phone_number = data[:homephone]
+    @street = data[:street]
+    @city = data[:city]
+    @state = data[:state]
+    @zipcode = data[:zipcode]
   end
 
   def name

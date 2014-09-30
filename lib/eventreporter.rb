7d446b49c3b4ba06fc58aedb7afdcc20dddc94e5
David Bayendor <dbayendor@gmail.com>
2014-09-30 08:11:27 -0600

    Modify initialize to load from ‘./data’ directory

--- lib/eventreporter.rb
+++ lib/eventreporter.rb
@@ -2,4 +2,12 @@
 
 Dir['./lib/*.rb'].each { |file| require file }
 
-CLI.new.start
+# CLI.new.start
+
+class EventReporter
+  attr_reader :repository
+
+  def initialize(repository = Repository.load_entries('./data'))
+    @repository = repository
+  end
+end

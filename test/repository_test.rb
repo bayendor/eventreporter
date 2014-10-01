6822da90379276062ee6ccd2db65aa5f85a71846
David Bayendor <dbayendor@gmail.com>
2014-10-01 14:43:21 -0600

    Refactor repository class
    * Use #send to simplify find_by
    
    * Refactor repository tests

--- test/repository_test.rb
+++ test/repository_test.rb
@@ -11,7 +11,7 @@ class RepositoryTest < Minitest::Test
     ].map { |row| Entry.new(row) }
 
     repository = Repository.new(entries)
-    entries = repository.find_by_first_name('Allison').sort_by(&:first_name)
+    entries = repository.find_by('first_name', 'Allison').sort_by(&:first_name)
     assert_equal 2, entries.length
   end
 
@@ -22,18 +22,18 @@ class RepositoryTest < Minitest::Test
     ].map { |row| Entry.new(row) }
 
     repository = Repository.new(entries)
-    entries = repository.find_by_last_name('Nguyen').sort_by(&:last_name)
+    entries = repository.find_by('last_name', 'Nguyen').sort_by(&:first_name)
     assert_equal 2, entries.length
   end
 
   def test_retrieve_by_zipcode
-    entries = [
+  entries = [
       { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' },
       { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' },
     ].map { |row| Entry.new(row) }
 
     repository = Repository.new(entries)
-    entries = repository.find_by_zipcode('20010').sort_by(&:zipcode)
+    entries = repository.find_by('zipcode','20010').sort_by(&:zipcode)
     assert_equal 2, entries.count
   end
 
@@ -44,25 +44,25 @@ class RepositoryTest < Minitest::Test
     ].map { |row| Entry.new(row) }
 
     repository = Repository.new(entries)
-    entries = repository.find_by_state('DC').sort_by(&:state)
+    entries = repository.find_by('state', 'DC').sort_by(&:state)
     assert_equal 2, entries.count
   end
 
   def test_case_insensitive_search
     entries = [
       { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' },
-      { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' },
+      { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' }
     ].map { |row| Entry.new(row) }
 
     repository = Repository.new(entries)
-    entries = repository.find_by_last_name('nguyen').sort_by(&:first_name)
+    entries = repository.find_by('state', 'DC').sort_by(&:state)
     assert_equal 2, entries.count
   end
 
   def test_empty_count
     entries = []
     repository = Repository.new(entries)
-    assert_equal 0, repository.find_count
+    assert_equal 0, repository.results_count
   end
 
   def test_non_empty_count
@@ -71,7 +71,7 @@ class RepositoryTest < Minitest::Test
       { regdate: '11/12/08 10:47', first_name: 'Allison', last_name: 'Nguyen', email_address: 'arannon@jumpstartlab.com', homephone: '6154385000', street: '3155 19th St NW', city: 'Washington', state: 'DC', zipcode: '20010' },
     ].map { |row| Entry.new(row) }
     repository = Repository.new(entries)
-    entries = repository.find_by_state('DC').sort_by(&:state)
-    assert_equal 2, repository.find_count
+    entries = repository.find_by('state', 'DC').sort_by(&:state)
+    assert_equal 2, repository.results_count
   end
 end

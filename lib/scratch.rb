9bed013fa969d06b62c15531a7cc47772e3ae5d3
David Bayendor <dbayendor@gmail.com>
2014-10-01 21:33:24 -0600

    Scratch: File Load Logic, table print example

--- lib/scratch.rb
+++ lib/scratch.rb
@@ -1,11 +1,10 @@
 require 'terminal-table'
 
-
 rows = []
 rows << ['One', 1]
 rows << ['Two', 2]
 rows << ['Three', 3]
-table = Terminal::Table.new :rows => rows
+table = Terminal::Table.new rows: rows
 
 puts table
 #
@@ -14,3 +13,15 @@ puts table
 # | Two   | 2 |
 # | Three | 3 |
 # +-------+---+
+
+# File Load Logic
+
+def load_file(file = 'event_attendees.csv')
+  if File.exist?("./data/#{file}")
+    data = FileHandler.new(file)
+    @repository = Repository.new(data.entries)
+    printer.loaded
+  else
+    printer.file_not_found
+  end
+end

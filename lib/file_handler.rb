2b04b4776e4caba37fae7279d8d6361b919cbf9a
David Bayendor <dbayendor@gmail.com>
2014-10-01 21:33:48 -0600

    Update file_handler to accept file name only

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -4,8 +4,8 @@ require_relative 'entry'
 class FileHandler
   attr_reader :entries
 
-  def initialize(directory='data/', file = 'event_test.csv')
-    file = File.join(directory, file)
+  def initialize(file = 'event_attendees.csv')
+    file = File.join('data/', file)
     data = CSV.open(file, headers: true, header_converters: :symbol)
     @entries = data.collect { |row| Entry.new(row) }
   end

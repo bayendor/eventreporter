52d960bcd5cfddd8fbc402d92b5ea2476018a3af
Kate Lane <katelane@gmail.com>
2014-10-01 22:17:46 -0600

    CLI wireup
    * Fixed queue command issues
    
    * Fixed abrupt quit issue
    
    * File still not loading so tests incomplete

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,10 +3,10 @@ require_relative 'repository'
 require_relative 'file_handler'
 
 class CLI
-  attr_reader :command, :printer, :repository, :input
+  attr_reader :command, :printer, :repository, :input, :file
 
   def initialize(output_stream)
-    file = FileHandler.new
+    @file = FileHandler.new
     entries = file.entries
     @printer = MessagePrinter.new(output_stream)
     @repository = Repository.new(entries)
@@ -16,30 +16,31 @@ class CLI
     printer.intro
     until finished?
       printer.command_request
-      process_command(gets)
+      process_command
     end
     printer.ending
   end
 
-  def process_command(input)
-    @input = input.strip.downcase.split(" ")
+  def process_command
+    @input = gets.strip.downcase.split(" ")
     @command = @input[0]
     case
     when load?
-      @repository = Repository.load_entries('./data')
-      printer.loaded
+      load_file(file)
     when queue_count
       count = @repository.results_count
       printer.results_count(count)
     when queue_clear
       @repository.results_clear
       printer.results_clear
+    when queue_print
+      @repository.results_print
+      printer.results_print
     when help?
       printer.help
       help_additional_options
     else
-      printer.not_a_valid_command
-      #this isn't working... why?
+      not_a_valid_command
     end
   end
 
@@ -70,13 +71,31 @@ class CLI
     command == "l" || command == "load"
   end
 
+  def load_file(file="event_attendees.csv")
+    if File.exist?("./data/#{file}")
+      data = FileHandler.new(file)
+      @repository = Repository.new(data.entries)
+      printer.loaded
+    else
+      printer.file_not_found
+    end
+  end
+
   def queue_clear
-    command == "a"
+    @input == ["queue", "clear"]
   end
 
   def queue_count
-    command == "c"
-    #"queue count" not working
+    @input == ["queue", "count"]
+  end
+
+  # def queue_sort
+  #   @input == ["queue", "sort"]
+  #   #when is this used?
+  # end
+
+  def queue_print
+    @input == ["queue", "print"]
   end
 
   def help?
@@ -111,8 +130,17 @@ class CLI
     @input == ["help", "save"]
   end
 
+  def not_a_valid_command
+    if command == "q" || command == "quit"
+      printer.ending
+      exit
+    else
+      printer.not_a_valid_command_message
+    end
+  end
+
   def finished?
-    command == "x" || command == "exit"
+    command == "q" || command == "quit"
   end
 
 end
